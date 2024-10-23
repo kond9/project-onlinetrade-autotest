8e28df4cf01e5d7c078c8d2da912b47aa5b9fcb5
kond9 <plenkin.2018@gmail.com>
2024-10-23 14:18:44 +0300

    Вписать аннотации внутрь функций

--- base/base_class.py
+++ base/base_class.py
@@ -2,15 +2,15 @@ class Base():
     def __init__(self, driver):
         self.driver = driver
 
-    """Метод получения текущего url"""
-
     def get_current_url(self):
+        """Метод получения текущего url"""
+
         get_url = self.driver.current_url
         print("Current url " + get_url)
 
-    """Метод проверки кодового слова"""
-
     def assert_word(self, word, result):
+        """Метод проверки кодового слова"""
+
         value_word = word.text
         assert value_word == result
         print("Проверочное слово сошлось")
